930a049291f3996177da0606d5fa5b26b29a84e8
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-08-09 17:56:40 -0500

    feat: Load Product entity into DB

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,12 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('products')
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  title: string;
+}
