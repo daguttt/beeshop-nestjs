eab696c3fca7c902e1133d944c2a90bcf1987e22
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-08-09 18:18:37 -0500

    feat: Add a product

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,7 @@
-export class CreateProductDto {}
+import { IsString, MinLength } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(4)
+  title: string;
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,15 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    const product = this.productRepository.create(createProductDto);
+    try {
+      await this.productRepository.save(product);
+      return product;
+    } catch (err) {
+      // In case the product already exists
+      if (err.code === '23505') throw new BadRequestException(err.detail);
+      throw new InternalServerErrorException('Unhandled exception');
+    }
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    return await this.productRepository.find();
   }
 
   findOne(id: number) {
