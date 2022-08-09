92be1455ef1d9e78d811a245850b09893da02dcc
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-08-09 17:34:20 -0500

    feat: Validate env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: [
+        '.env',
+        '.env.development',
+        '.env.staging',
+        '.env.production',
+      ],
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'staging', 'test', 'production')
+          .default('development'),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+        DATABASE_USERNAME: Joi.string().required(),
+      }),
     }),
   ],
 })
