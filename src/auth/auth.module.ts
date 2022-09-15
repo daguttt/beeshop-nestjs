3caaec61ff378e8adbd0359fdc41ffab5b56c5ef
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-09-15 18:37:57 -0500

    feat: Install and configure Passport and JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,42 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
+import { User } from 'src/users/entities/user.entity';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { User } from 'src/users/entities/user.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        // console.log('JWT SECRET', process.env.JWT_SECRET);
+        // console.log('JWT SECRET', configService.get('JWT_SECRET'));
+        return {
+          secret: configService.get('JWT_SECRET'),
+          // secret: process.env.JWT_SECRET,
+          signOptions: {
+            expiresIn: '2h',
+          },
+        };
+      },
+    }),
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: {
+    //     expiresIn: '2h',
+    //   },
+    // }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
